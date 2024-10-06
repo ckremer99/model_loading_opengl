cf70f1b86c0c3ebfe337fc06aa580bbed36d3697
ckremer99 <ckremer99@outlook.com>
2024-10-06 07:30:07 -0700

    Initial Commit

---
+++ model_loading_opengl/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  model_loading_opengl
+//
+//  Created by Cody Kremer on 10/6/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
